d3bc9a6d066adc2775c94a2193d0f620e7ada3ce
Mayank Jain <jainmayank0@gmail.com>
2018-01-21 14:20:59 +0530

    added ',' also as the default in special letters.
    I was using this but had some trouble as all the ',' were not visible after this effect, so I have added ',' in the special character section for effeciency.

--- birdman.js
+++ birdman.js
@@ -82,7 +82,7 @@
 		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 		var capitalAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 		var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-		var specialChars = ['-','+','*','_','~','<','>','|','.','?','!', '§','$','%','&','quot','/','(',')','[',']','{','}','='];
+		var specialChars = ['-','+','*','_','~','<','>','|','.',',','?','!', '§','$','%','&','quot','/','(',')','[',']','{','}','='];
 		var combinedAlphabet = zip(alphabet, capitalAlphabet).concat(numbers).concat([specialChars]);
 		params.order = defaultFor(params.order, combinedAlphabet);
 		params.delay = defaultFor(params.delay, 150);
